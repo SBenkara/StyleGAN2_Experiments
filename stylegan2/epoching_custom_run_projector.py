7faefbce297e2186b6fafddd537c190a70717972
ALIEN <venum772@gmail.com>
2020-11-21 11:24:13 -0600

    added few changes to the training layer

--- stylegan2/epoching_custom_run_projector.py
+++ stylegan2/epoching_custom_run_projector.py
@@ -11,7 +11,8 @@ import dnnlib.tflib as tflib
 import re
 import sys
 
-import projector
+import projector_modified as projector
+#import projector
 import pretrained_networks
 from training import dataset
 from training import misc
